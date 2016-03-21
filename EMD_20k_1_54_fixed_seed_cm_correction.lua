69c317e1d5ec4219e91b36457f34835a41bef722
Sidd <shelts@rpi.edu>
2016-03-21 14:14:15 -0400

    update

--- EMD_20k_1_54_fixed_seed_cm_correction.lua
+++ EMD_20k_1_54_fixed_seed_cm_correction.lua
@@ -1,10 +1,11 @@
 arg = { ... }
 
-assert(#arg == 6, "Expected 6 arguments")
+assert(#arg == 7, "Expected 6 arguments")
 assert(argSeed ~= nil, "Expected seed")
 argSeed = 34086709
-prng = DSFMT.create(argSeed)
 print("seed: ", argSeed)
+prng = DSFMT.create(argSeed)
+
 -- npa = new parameter arrangement --
 
 evolveTime       = tonumber(arg[1])
@@ -13,12 +14,20 @@ rscale_l         = tonumber(arg[3])
 light_r_ratio    = tonumber(arg[4])
 mass_l           = tonumber(arg[5])
 light_mass_ratio = tonumber(arg[6])
+version          = tonumber(arg[7])
+
+print('version: ', version)
+if version == 0 then
+    correcting = 'y'
+elseif version == 1 then
+    correcting = 'n'
+end
 
 model1Bodies = 20000
 totalBodies = model1Bodies
 
 nbodyLikelihoodMethod = "EMD"
-nbodyMinVersion = "1.56"
+nbodyMinVersion = "1.54"
 
 --fitting ml directly. mass ratio as originally defined
 --radius ratio now defined the way mass ratio is.
@@ -29,7 +38,8 @@ dwarfMass = mass_l / light_mass_ratio
 rscale_t  = rscale_l / light_r_ratio
 rscale_d  = rscale_t *  (1.0 - light_r_ratio)
 mass_d    = dwarfMass * (1.0 - light_mass_ratio)
--- print("evolve time: ", evolveTime)
+
+--print(evolveTime)
 -- print(mass_d, mass_l)
 function makePotential()
    return  Potential.create{
@@ -42,6 +52,7 @@ end
 function get_timestep()
     --Mass of a single dark matter sphere enclosed within light rscale
     mass_enc_d = mass_d * (rscale_l)^3 * ( (sqr(rscale_l)+ sqr(rscale_d) ) )^(-3.0/2.0)
+
     --Mass of a single light matter sphere enclosed within dark rscale
     mass_enc_l = mass_l * (rscale_d)^3 * ( (sqr(rscale_l)+ sqr(rscale_d) ) )^(-3.0/2.0)
 
@@ -73,8 +84,7 @@ function makeContext()
    }
 end
 
-function cm_calc(firstModel, dwarf_starting_position, dwarf_starting_velocity)
-
+function cm_correction(firstModel, finalPosition, finalVelocity)
     cm_x = 0.0
     cm_y = 0.0
     cm_z = 0.0
@@ -90,8 +100,13 @@ function cm_calc(firstModel, dwarf_starting_position, dwarf_starting_velocity)
         cm_vx = cm_vx + ( firstModel[i].mass * firstModel[i].velocity.x) 
         cm_vy = cm_vy + ( firstModel[i].mass * firstModel[i].velocity.y) 
         cm_vz = cm_vz + ( firstModel[i].mass * firstModel[i].velocity.z) 
---         print('old positions: ' , firstModel[i].position.x, firstModel[i].position.y, firstModel[i].position.z)
+        if correcting == 'n' then
+            print(firstModel[i].position.x, firstModel[i].position.y, firstModel[i].position.z, firstModel[i].velocity.x, firstModel[i].velocity.y, firstModel[i].velocity.z)
+        end
+        
+--         print('old positions: ', firstModel[i].position.x, firstModel[i].position.y, firstModel[i].position.z)
 --         print('old velocities: ', firstModel[i].velocity.x, firstModel[i].velocity.y, firstModel[i].velocity.z)
+--         print(firstModel[i].mass)
     end
     cm_x  = cm_x  / dwarfMass
     cm_y  = cm_y  / dwarfMass
@@ -99,35 +114,79 @@ function cm_calc(firstModel, dwarf_starting_position, dwarf_starting_velocity)
     cm_vx = cm_vx / dwarfMass
     cm_vy = cm_vy / dwarfMass
     cm_vz = cm_vz / dwarfMass
-    print('new CM: ', cm_x, cm_y, cm_z)
-    print('new CMV: ', cm_vx, cm_vy, cm_vz)
+--     print('old CM: ', cm_x, cm_y, cm_z)
+--     print('old CMV: ', cm_vx, cm_vy, cm_vz)
+    
+    if correcting == 'y' then
+--         print("correcting: ")
+        for i, v in ipairs(firstModel)
+        do
+            x_new = firstModel[i].position.x - cm_x + finalPosition.x
+            y_new = firstModel[i].position.y - cm_y + finalPosition.y
+            z_new = firstModel[i].position.z - cm_z + finalPosition.z
+            
+            vx_new = firstModel[i].velocity.x - cm_vx + finalVelocity.x
+            vy_new = firstModel[i].velocity.y - cm_vy + finalVelocity.y
+            vz_new = firstModel[i].velocity.z - cm_vz + finalVelocity.z
+            
+            firstModel[i].position = Vector.create(x_new, y_new, z_new)
+            firstModel[i].velocity = Vector.create(vx_new, vy_new, vz_new)
+        end
+    end
+    cm_x = 0.0
+    cm_y = 0.0
+    cm_z = 0.0
+    cm_vx = 0.0
+    cm_vy = 0.0
+    cm_vz = 0.0
+    t_mass = 0.0
+    for i, v in ipairs(firstModel)
+    do
+        cm_x  = cm_x  + ( firstModel[i].mass * firstModel[i].position.x) 
+        cm_y  = cm_y  + ( firstModel[i].mass * firstModel[i].position.y) 
+        cm_z  = cm_z  + ( firstModel[i].mass * firstModel[i].position.z) 
+        
+        cm_vx = cm_vx + ( firstModel[i].mass * firstModel[i].velocity.x) 
+        cm_vy = cm_vy + ( firstModel[i].mass * firstModel[i].velocity.y) 
+        cm_vz = cm_vz + ( firstModel[i].mass * firstModel[i].velocity.z) 
+        if correcting == 'y' then
+            print(firstModel[i].position.x, firstModel[i].position.y, firstModel[i].position.z, firstModel[i].velocity.x, firstModel[i].velocity.y, firstModel[i].velocity.z)
+        end
+--         print('new positions: ' , firstModel[i].position.x, firstModel[i].position.y, firstModel[i].position.z)
+--         print('new velocities: ', firstModel[i].velocity.x, firstModel[i].velocity.y, firstModel[i].velocity.z)
+        t_mass = t_mass + firstModel[i].mass
+    end
+    
+    print(t_mass)
+    cm_x  = cm_x  / dwarfMass
+    cm_y  = cm_y  / dwarfMass
+    cm_z  = cm_z  / dwarfMass
+    cm_vx = cm_vx / dwarfMass
+    cm_vy = cm_vy / dwarfMass
+    cm_vz = cm_vz / dwarfMass
+--     print('new CM: ', cm_x, cm_y, cm_z)
+--     print('new CMV: ', cm_vx, cm_vy, cm_vz)
   return firstModel
 end
 
 -- Also required
 -- position  = lbrToCartesian(ctx, Vector.create(218, 53.5, 28.6))
--- velocity  = Vector.create(-156, 79, 107),
 function makeBodies(ctx, potential)
   local firstModel
-  --this puts a particle at the current postion and velocity of the Orphan stream
-  --calculates the reverse orbit along the potential and finds the required
-  --center of mass position and velocity.
-  local dwarf_starting_position, dwarf_starting_velocity = reverseOrbit{
+  local finalPosition, finalVelocity = reverseOrbit{
       potential = potential,
       position  = lbrToCartesian(ctx, Vector.create(218, 53.5, 28.6)),
       velocity  = Vector.create(-156, 79, 107),
       tstop     = reverseOrbitTime,
-      dt        = ctx.timestep / 10.0,
+      dt        = ctx.timestep / 10.0
   }
-    print('final position: ', dwarf_starting_position.x, dwarf_starting_position.y, dwarf_starting_position.z)
-    print('final velocity: ',dwarf_starting_velocity.x, dwarf_starting_velocity.y, dwarf_starting_velocity.z)
---     print("position: ", lbrToCartesian(ctx, Vector.create(218, 53.5, 28.6)))
---     print("velocity: ", Vector.create(-156, 79, 107))
+    print('final position: ', finalPosition.x, finalPosition.y, finalPosition.z)
+    print('final velocity: ',finalVelocity.x, finalVelocity.y, finalVelocity.z)
   firstModel = predefinedModels.isotropic{
       nbody       = model1Bodies,
       prng        = prng,
-      position    = dwarf_starting_position,
-      velocity    = dwarf_starting_velocity,
+      position    = finalPosition,
+      velocity    = finalVelocity,
       mass1       = mass_l,
       mass2       = mass_d,
       scaleRadius1 = rscale_l,
@@ -135,12 +194,10 @@ function makeBodies(ctx, potential)
       ignore      = true
   }
   
-  cm_calc(firstModel, dwarf_starting_position, dwarf_starting_velocity)
+  cm_correction(firstModel, finalPosition, finalVelocity)
 
   return firstModel
 end
-
-
 
 function makeHistogram()
     return HistogramParams.create{
@@ -156,3 +213,5 @@ function makeHistogram()
      betaBins = 1
 }
 end
+
+
--- EMD_20k_v158_fixed_seed.lua
+++ EMD_20k_v158_fixed_seed.lua
@@ -3,10 +3,7 @@ arg = { ... }
 assert(#arg == 6, "Expected 6 arguments")
 assert(argSeed ~= nil, "Expected seed")
 argSeed = 34086709
--- argSeed = 86043093
-print("seed: ", argSeed)
 prng = DSFMT.create(argSeed)
-
 -- npa = new parameter arrangement --
 
 evolveTime       = tonumber(arg[1])
@@ -20,7 +17,7 @@ model1Bodies = 20000
 totalBodies = model1Bodies
 
 nbodyLikelihoodMethod = "EMD"
-nbodyMinVersion = "1.56"
+nbodyMinVersion = "1.58"
 
 --fitting ml directly. mass ratio as originally defined
 --radius ratio now defined the way mass ratio is.
