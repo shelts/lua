2dbb95f7e9a29ad8466901f53fa38eec8b7b80f9
Sidd <shelts@rpi.edu>
2016-06-08 15:08:31 -0400

    update

--- EMD_v162.lua
+++ EMD_v162.lua
@@ -24,7 +24,7 @@ rscale_l         = round( rscale_l,         dec )
 light_r_ratio    = round( light_r_ratio,    dec )
 mass_l           = round( mass_l,           dec )
 light_mass_ratio = round( light_mass_ratio, dec )
-model1Bodies = 20000
+model1Bodies = 200
 totalBodies = model1Bodies
 
 nbodyLikelihoodMethod = "EMD"
@@ -36,26 +36,32 @@ rscale_t  = rscale_l / light_r_ratio
 rscale_d  = rscale_t *  (1.0 - light_r_ratio)
 mass_d    = dwarfMass * (1.0 - light_mass_ratio)
 
-evolveTime = 2.0
-revOrbTime = 2.0
-
-dwarfMass = 5e6 / 222288.47
-mass_ratio = 1/10
-mass_d = dwarfMass / (mass_ratio + 1)
-mass_l = dwarfMass - mass_d
-mass_d = 2.0 * mass_d
-rscale_d = .25
-r_ratio = 1.0/5.0
-rscale_l = rscale_d * r_ratio
-rscale_l = 0.025
-rscale_d = 0.3
-
-print_reverse_orbit = true
+charles_run = false
+print_reverse_orbit = false
 print('forward time=', evolveTime, '\nrev time=',  revOrbTime)
 print('mass_l sim=', mass_l, '\nmass_d sim=', mass_d)
-print('light mass solar=', mass_l * 222288.47, '\ndark mass solar=', mass_d * 222288.47)
-print('total mass solar= ', (mass_d + mass_l) * 222288.47)
 print('rl = ', rscale_l, 'rd = ', rscale_d)
+if(charles_run == true) then
+    evolveTime = 2.0
+    revOrbTime = 2.0
+    
+    dwarfMass = 5e6 / 222288.47
+    mass_ratio = 1/10
+    mass_d = dwarfMass / (mass_ratio + 1)
+    mass_l = dwarfMass - mass_d
+    mass_d = 2.0 * mass_d
+    rscale_d = .25
+    r_ratio = 1.0/5.0
+    rscale_l = rscale_d * r_ratio
+    rscale_l = 0.025
+    rscale_d = 0.3
+
+    print('forward time=', evolveTime, '\nrev time=',  revOrbTime)
+    print('mass_l sim=', mass_l, '\nmass_d sim=', mass_d)
+    print('light mass solar=', mass_l * 222288.47, '\ndark mass solar=', mass_d * 222288.47)
+    print('total mass solar= ', (mass_d + mass_l) * 222288.47)
+    print('rl = ', rscale_l, 'rd = ', rscale_d)
+end
 
 -- function makePotential()
 --    return  Potential.create{
@@ -95,7 +101,7 @@ function get_timestep()
     t = (1 / 100.0) * ( pi_4_3 * s)^(1.0/2.0)
     
 --     t = sqr(1/10.0) * sqrt((pi_4_3 * cube(rscale_d)) / (dwarfMass))
-    print('timestep ', t)
+--     print('timestep ', t)
     
     return t
 end
@@ -112,8 +118,11 @@ function makeContext()
       theta      = 1.0
    }
 end
-soften_length = (mass_l * rscale_l + mass_d  * rscale_d) / (mass_d + mass_l)
-print('soften_length ', calculateEps2(totalBodies, soften_length ))
+
+if(charles_run == true) then
+    soften_length = (mass_l * rscale_l + mass_d  * rscale_d) / (mass_d + mass_l)
+    print('soften_length ', calculateEps2(totalBodies, soften_length ))
+end
 -- Also required
 -- for orphan: lbr in sun centered
 -- position  = lbrToCartesian(ctx, Vector.create(218, 53.5, 28.6))
@@ -135,13 +144,16 @@ function makeBodies(ctx, potential)
         position  = lbrToCartesian(ctx, Vector.create(45, 46.93, 11.87)),
         velocity  = Vector.create(-122.78, 157.32, 64.90),
         tstop     = .14,
+        tstopf    = .20,
         dt        = ctx.timestep / 10.0
     }
     print('Printing reverse orbit')
   end
-  print(lbrToCartesian(ctx, Vector.create(45, 46.93, 11.87)), Vector.create(-122.78, 157.32, 64.90))
-  print(finalPosition, finalVelocity)
   
+  if(charles_run == true) then
+    print(lbrToCartesian(ctx, Vector.create(45, 46.93, 11.87)), Vector.create(-122.78, 157.32, 64.90))
+    print(finalPosition, finalVelocity)
+  end
   firstModel = predefinedModels.isotropic{
       nbody       = model1Bodies,
       prng        = prng,
