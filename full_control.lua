91634ab338a1f95856c73da5a37f064d0acec178
shelts <shelts@rpi.edu>
2018-06-19 13:38:36 -0400

    deprecated old files

--- full_control.lua
+++ full_control.lua
@@ -1,4 +1,4 @@
--- /* Copyright (c) 2016 Siddhartha Shelton */
+-- /* Copyright (c) 2016-2018 Siddhartha Shelton */
 
 -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- 
 -- DEAR LUA USER:
@@ -7,6 +7,7 @@
 -- Many of these the client will not need.
 
 -- NOTE --
+-- to fully utilize this lua, need to compile with -DNBODY_DEV_OPTIONS=ON
 -- if you are using single component plummer model, it will take the baryonic
 -- matter component parameters. meaning you input should look like
 -- ft, bt, rscale_baryon, radius_ratio, baryon mass, mass ratio
@@ -21,12 +22,27 @@ totalBodies           = 20000   -- -- NUMBER OF BODIES           -- --
 nbodyLikelihoodMethod = "EMD"   -- -- HIST COMPARE METHOD        -- --
 nbodyMinVersion       = "1.68"  -- -- MINIMUM APP VERSION        -- --
 
-run_null_potential    = false   -- -- NULL POTENTIAL SWITCH      -- --
-two_component_model   = true    -- -- TWO COMPONENTS SWITCH      -- --
+run_null_potential    = true   -- -- NULL POTENTIAL SWITCH      -- --
 use_tree_code         = true    -- -- USE TREE CODE NOT EXACT    -- --
 print_reverse_orbit   = false   -- -- PRINT REVERSE ORBIT SWITCH -- --
-print_out_parameters  = false    -- -- PRINT OUT ALL PARAMETERS   -- --
+print_out_parameters  = true    -- -- PRINT OUT ALL PARAMETERS   -- --
 -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- 
+
+
+
+-- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- 
+-- -- -- -- -- -- -- -- -- MODEL SETTINGS   -- -- -- -- -- -- -- -- -- --        
+-- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- 
+-- -- ModelComponent Options: 
+-- --       2 - TWO COMPONENT MODEL     -- -- -- -- -- -- -- -- -- -- 
+-- --       1 - SINGLE COMPONENT MODEL  -- -- -- -- -- -- -- -- -- -- 
+-- --       0 - NO DWARF MODEL          -- -- -- -- -- -- -- -- -- -- 
+ModelComponents   = 2       -- -- TWO COMPONENTS SWITCH      -- --
+manual_bodies     = false    -- -- USE THE MANUAL BODY LIST   -- --
+-- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- 
+
+
+
 
 -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- 
 -- -- -- -- -- -- -- -- -- PARAMETER SETTINGS   -- -- -- -- -- -- -- -- -- --
@@ -43,8 +59,8 @@ bta_bins        = 1       -- number of beta bins. normally use 1 for 1D hist
 bta_lower_range = -15     -- lower range for beta
 bta_upper_range = 15      -- upper range for beta
 
-SigmaCutoff          = 2.5     -- -- sigma cutoff for outlier rejection -- --
-Correction           = 1.111   -- -- correction for outlier rejection -- --
+SigmaCutoff          = 2.5     -- -- sigma cutoff for outlier rejection DO NOT CHANGE -- --
+Correction           = 1.111   -- -- correction for outlier rejection   DO NOT CHANGE -- --
 -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- 
 
 -- -- -- -- -- -- -- -- -- AlGORITHM OPTIONS -- -- -- -- -- -- -- --
@@ -60,11 +76,11 @@ use_vel_disps        = false   -- use velocity dispersions in likelihood
 -- -- -- -- -- -- These options only work if you compile nbody with  -- -- --
 -- -- -- -- -- -- the -DNBODY_DEV_OPTIONS set to on                  -- -- --   
 
-useMultiOutputs       = false   -- -- WRITE MULTIPLE OUTPUTS       -- --
-freqOfOutputs         = 6       -- -- FREQUENCY OF WRITING OUTPUTS -- --
+useMultiOutputs       = false    -- -- WRITE MULTIPLE OUTPUTS       -- --
+freqOfOutputs         = 6        -- -- FREQUENCY OF WRITING OUTPUTS -- --
 
-timestep_control     = false    -- -- control number of steps      -- --
-Ntime_steps          = 1        -- -- number of timesteps to run   -- --
+timestep_control     = true     -- -- control number of steps      -- --
+Ntime_steps          = 0        -- -- number of timesteps to run   -- --
 -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- 
         
 
@@ -122,7 +138,7 @@ function get_timestep()
     else 
         t = sqr(1/10.0) * sqrt((pi_4_3 * cube(rscale_l)) / (mass_l))
     end
---     print("LUA FILE: ", t)
+--     print(t)
     return t
 end
 
@@ -137,24 +153,25 @@ function makeContext()
       useQuad     = true,
       useBestLike   = use_best_likelihood,
       BestLikeStart = best_like_start,
-      useBetaDisp   = use_beta_disps,
       useVelDisp    = use_vel_disps,
+      useBetaDisp   = use_beta_disps,
       Nstep_control = timestep_control,
       Ntsteps       = Ntime_steps,
-      MultiOutput   = useMultiOutputs,
-      OutputFreq    = freqOfOutputs,
       BetaSigma     = SigmaCutoff,
       VelSigma      = SigmaCutoff,
       BetaCorrect   = Correction,
       VelCorrect    = Correction,
+      MultiOutput   = useMultiOutputs,
+      OutputFreq    = freqOfOutputs,
       theta       = 1.0
    }
 end
 
 
+
 function makeBodies(ctx, potential)
   local firstModel
-  local finalPosition, finalVelocity, finalPosition2, finalVelocity2
+  local finalPosition, finalVelocity
     if(run_null_potential == true) then
         print("placing dwarf at origin")
         finalPosition, finalVelocity = Vector.create(0, 0, 0), Vector.create(0, 0, 0)
@@ -166,16 +183,6 @@ function makeBodies(ctx, potential)
             tstop     = revOrbTime,
             dt        = ctx.timestep / 10.0
             }
-            
---         finalPosition2, finalVelocity2 = reverseOrbit2{
---             potential = potential,
---             position  = lbrToCartesian(ctx, Vector.create(orbit_parameter_l, orbit_parameter_b, orbit_parameter_r)),
---             velocity  = Vector.create(orbit_parameter_vx, orbit_parameter_vy, orbit_parameter_vz),
---             tstop     = revOrbTime,
---             dt        = ctx.timestep / 10.0
---             }
---         print(finalPosition, finalVelocity)
---         print(finalPosition2, finalVelocity2)
     end
     
     if(print_reverse_orbit == true) then
@@ -192,7 +199,7 @@ function makeBodies(ctx, potential)
     
 
   
-    if(two_component_model) then 
+    if(ModelComponents == 2) then 
         firstModel = predefinedModels.mixeddwarf{
             nbody       = totalBodies,
             prng        = prng,
@@ -202,8 +209,8 @@ function makeBodies(ctx, potential)
             comp2       = Dwarf.plummer{mass = mass_d, scaleLength = rscale_d}, -- Dwarf Options: plummer, nfw, general_hernquist
             ignore      = true
         }
-       
-    else
+        
+    elseif(ModelComponents == 1) then
         firstModel = predefinedModels.plummer{
             nbody       = totalBodies,
             prng        = prng,
@@ -216,7 +223,23 @@ function makeBodies(ctx, potential)
   
     end
   
-  return firstModel
+    if(manual_bodies) then
+        manualModel = predefinedModels.manual_bodies{
+        body_file   = manual_body_file,
+    }
+         
+    end
+    
+    if(ModelComponents > 0 and manual_bodies) then 
+        return firstModel, manualModel
+    elseif(ModelComponents == 0 and manual_bodies) then
+        return manualModel
+    elseif(ModelComponents > 0 and not manual_bodies) then
+        return firstModel
+    else    
+        print("Don't you want to simulate something?")
+    end
+    
 end
 
 function makeHistogram()
@@ -239,10 +262,9 @@ end
 
 
 arg = { ... } -- -- TAKING USER INPUT
-assert(#arg == 6, "Expected 6 arguments")
+assert(#arg >= 6, "Expected 6 arguments")
 assert(argSeed ~= nil, "Expected seed") -- STILL EXPECTING SEED AS INPUT FOR THE FUTURE
--- argSeed = 34086709 -- -- SETTING SEED TO FIXED VALUE
-argSeed = 7854614814 -- -- SETTING SEED TO FIXED VALUE Seed used for mw@home
+argSeed = 34086709 -- -- SETTING SEED TO FIXED VALUE
 prng = DSFMT.create(argSeed)
 
 -- -- -- -- -- -- -- -- -- ROUNDING USER INPUT -- -- -- -- -- -- -- --
@@ -259,6 +281,7 @@ rscale_l         = round( tonumber(arg[3]), dec )
 light_r_ratio    = round( tonumber(arg[4]), dec )
 mass_l           = round( tonumber(arg[5]), dec )
 light_mass_ratio = round( tonumber(arg[6]), dec )
+manual_body_file = arg[7]
 
 -- -- -- -- -- -- -- -- -- DWARF PARAMETERS   -- -- -- -- -- -- -- --
 revOrbTime = evolveTime
@@ -267,9 +290,19 @@ rscale_t  = rscale_l / light_r_ratio
 rscale_d  = rscale_t *  (1.0 - light_r_ratio)
 mass_d    = dwarfMass * (1.0 - light_mass_ratio)
 
+if(manual_bodies and manual_body_file == nil) then 
+    print 'WARNING: No body list given. Manual body input turn off'
+    manual_bodies = false  --optional body list was not included
+elseif(manual_bodies and ModelComponents == 0) then
+    print 'Using user inputted body list only' 
+    print( manual_body_file)
+elseif(manual_bodies and ModelComponents ~= 0) then
+    print 'Using dwarf model and user inputted body list'
+end
+
+
 if(use_tree_code) then
     criterion = "TreeCode"
---     criterion = "NewCriterion"
 else
     criterion = "Exact"
 end
